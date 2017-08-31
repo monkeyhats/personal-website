fcbe94f6a60fdd51c00e0bf371cd863440700423
monkeyhats <31523505+monkeyhats@users.noreply.github.com>
2017-08-31 19:27:50 -0400

    Update derek.js

--- derek.js
+++ derek.js
@@ -1,12 +1,10 @@
 $(document).ready(function(){
-    $(".click-to-slide").click(function(){
-        $("div#slide-in-nav").toggleClass("active");
-		$("#menu-button").toggleClass("menu-open");
-			
+	$(".click-to-slide").click(function(){
+        	$("div#slide-in-nav").toggleClass("active");
+			$("#menu-button").toggleClass("menu-open");
     });
 
 	$('div#side-menu a').hover(function(){		
-
 		var menu_id = $(this).attr('data-tab');
 
 		$('div#side-menu a').removeClass('current');
